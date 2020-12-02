481b9c91a9ee662db7e7d4cb876280ca5fa2a283
Adam Michalski <aadam.michalski@gmail.com>
2020-12-02 10:24:43 +0000

    Add template

---
+++ template.py
@@ -0,0 +1,20 @@
+INPUT = ""
+
+
+def get_input():
+    with open(INPUT, 'r') as f:
+        pass
+
+
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+
+
+if __name__ == "__main__":
+    inp = get_input()
+    print(f"part1: {part1(inp)}")
+    print(f"part2: {part2(inp)}")
